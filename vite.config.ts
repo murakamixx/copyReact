675ee56e27970248a7fd0b0796654964f03df4e8
murakamixx <kirill24053@gmail.com>
2025-04-30 02:17:22 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/luxuryhotelsreact",
+  base: "/copyReact",
   plugins: [react(),tailwindcss()],
 })
